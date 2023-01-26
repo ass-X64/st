fe65063ff50d27f3491a0c4fe93ef3b9aa4c1855
ass-X64 <rushabhpatil6969@proton.me>
2023-01-26 14:59:12 +0530

    disabled alpha patch

--- config.def.h
+++ config.def.h
@@ -118,7 +118,7 @@ char *termname = "st";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 1;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
